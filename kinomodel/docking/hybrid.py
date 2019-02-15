08b3ea88459092d0ccbbae216346af8220535eb3
John Chodera <john.chodera@choderalab.org>
2019-02-15 00:12:49 -0500

    Add basic hybrid docking test

--- kinomodel/docking/hybrid.py
+++ kinomodel/docking/hybrid.py
@@ -4,7 +4,7 @@ def hybrid_docking(receptor_path, molecules_path, docked_molecules_path, n_poses
     Parameters
     ----------
     receptor_path : str
-        Path to file containing receptor and reference ligand.
+        Path to PDB file containing receptor and reference ligand, or pre-prepared receptor for docking
     molecules_path : str
         Path to file containing one or more molecules (in OpenEye readable format) to be docked.
         (For example, list of SMILES)
@@ -13,6 +13,8 @@ def hybrid_docking(receptor_path, molecules_path, docked_molecules_path, n_poses
         Uses OpenEye recognized file extension, such as .mol2 or .sdf
     n_poses : int, optional, default=1
         Number of docked poses to generate
+    receptor_filename : str, optional, default=None
+        If not None, the pre-prepared receptor is loaded
 
     TODO: How can this API be improved?
 
@@ -21,28 +23,47 @@ def hybrid_docking(receptor_path, molecules_path, docked_molecules_path, n_poses
     from openeye import oedocking, oechem
     import openmoltools as moltools
 
-    # Load receptor
-    complex_istream = oechem.oemolistream(receptor_path)
-    complex = oechem.OEGraphMol()
-    oechem.OEReadMolecule(complex_istream, complex)
+    # Try to load pre-prepared receptor from specified file
+    receptor = oechem.OEGraphMol()
+    print('Attempting to load receptor from {}...'.format(receptor_path))
+    if not oedocking.OEReadReceptorFile(receptor, receptor_path):
+        # Load complex of receptor and reference ligand
+        complex_istream = oechem.oemolistream(receptor_path)
+        complex = oechem.OEGraphMol()
+        oechem.OEReadMolecule(complex_istream, complex)
 
-    # for input molecule inmol ...
-    ligand = oechem.OEGraphMol()
-    protein = oechem.OEGraphMol()
-    water = oechem.OEGraphMol()
-    other = oechem.OEGraphMol()
+        # Attempt to split into components and build receptor based on reference ligand
+        print('Attempting to split complex into components...')
+        ligand = oechem.OEGraphMol()
+        protein = oechem.OEGraphMol()
+        water = oechem.OEGraphMol()
+        other = oechem.OEGraphMol()
+        if oechem.OESplitMolComplex(ligand, protein, water, other, complex):
+            # Create receptor using bound ligand reference
+            print('Creating receptor using reference ligand...')
+            oedocking.OEMakeReceptor(receptor, protein, ligand)
+            # TODO: We can store prepared receptor file if desired
+            # oedocking.OEWriteReceptorFile(receptor, output_receptor_filename)
 
-    if oechem.OESplitMolComplex(ligand, protein, water, other, complex):
-        # Create receptor using bound ligand reference
-        receptor = oechem.OEGraphMol()
-        oedocking.OEMakeReceptor(receptor, protein, ligand)
+        else:
+            raise Exception('Could not split specified PDB file {} into receptor and reference ligand'.format(receptor_path))
 
-        # Open file for writing docked molecules
-        docked_molecules_ostream = oechem.oemolostream(docked_molecules_path)
+    # Open file for writing docked molecules
+    docked_molecules_ostream = oechem.oemolostream(docked_molecules_path)
 
-        # Dock all molecules requestd
-        molecules_istream = oechem.oemolistream(molecules_path)
-        molecule = oechem.OEGraphMol()
-        while oechem.OEReadMolecule(molecules_istream, molecule):
-            docked_molecules = pose_molecule(receptor, molecule, n_poses=n_poses)
-            oechem.OEWriteMolecule(docked_molecules_ostream, docked_molecules)
+    # Dock all molecules requested
+    dock_method = oedocking.OEDockMethod_Hybrid2
+    dock_resolution = oedocking.OESearchResolution_Standard
+    dock = oedocking.OEDock(dock_method, dock_resolution)
+    dock.Initialize(receptor)
+    molecules_istream = oechem.oemolistream(molecules_path)
+    molecule = oechem.OEGraphMol()
+    for molecule in molecules_istream.GetOEMols():
+        print("docking", molecule.GetTitle())
+        docked_molecule = oechem.OEGraphMol()
+        #docked_molecules = pose_molecule(receptor, molecule, n_poses=n_poses)
+        dock.DockMultiConformerMolecule(docked_molecule, molecule)
+        sdtag = oedocking.OEDockMethodGetName(dock_method)
+        oedocking.OESetSDScore(docked_molecule, dock, sdtag)
+        dock.AnnotatePose(docked_molecule)
+        oechem.OEWriteMolecule(docked_molecules_ostream, docked_molecule)
