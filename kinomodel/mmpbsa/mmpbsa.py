1767ddca10ce423cc8b67f1892f3363f1348cefd
David W Wright <dave.william.wright@gmail.com>
2019-03-26 15:55:29 +0000

    Draft working version of the code

--- kinomodel/mmpbsa/mmpbsa.py
+++ kinomodel/mmpbsa/mmpbsa.py
@@ -1,4 +1,7 @@
-def mmpbsa(inpcrd_filenames, prmtop_filenames, niterations=1000):
+import numpy as np
+
+
+def mmpbsa(inpcrd_filenames, prmtop_filenames, niterations=1000, implicit=True):
     """
     Run simple MM-PBSA like workflow to estimate free energy of binding from enthalpies of interaction.
 
@@ -32,14 +35,20 @@ def mmpbsa(inpcrd_filenames, prmtop_filenames, niterations=1000):
     timestep = 4.0 * unit.femtoseconds
     pressure = 1.0 * unit.atmospheres
 
+    if implicit:
+        method = app.CutoffNonPeriodic
+    else:
+        method = app.PME
+
     # Store thermal energy
-    kT = unit.kB_BOLTZMANN_CONSTANT * temperature
+    kT = unit.BOLTZMANN_CONSTANT_kB * temperature
 
     # TODO: Autodetect nonbondedMethod
 
     # Termination criteria
     nsteps_per_iteration = 250 # 1 picosecond
     # TODO: Use statistical error threshold to drive this to convergence
+    phases = inpcrd_filenames.keys()
 
     enthalpies = dict()
     for phase in phases:
@@ -48,21 +57,43 @@ def mmpbsa(inpcrd_filenames, prmtop_filenames, niterations=1000):
         # Load Amber system
         prmtop = app.AmberPrmtopFile(prmtop_filenames[phase])
         inpcrd = app.AmberInpcrdFile(inpcrd_filenames[phase])
+
+        if not implicit:
+            box = inpcrd.getBoxVectors()
+            context.setPeriodicBoxVectors(a=box[0], b=box[1], c=box[2])
+
+        cutoff = 2*unit.nanometers
+
         # Create system
-        system = prmtop.createSystem(nonbondedMethod=app.PME, constraints=app.Hbonds, hydrogenMass=hydrogen_mass)
+        system = prmtop.createSystem(nonbondedMethod=method,
+                                     constraints=app.HBonds,
+                                     nonbondedCutoff=cutoff,
+                                     hydrogenMass=hydrogen_mass)
+
         # Create integrator and context
         integrator = openmm.LangevinIntegrator(temperature, collision_rate, timestep)
         context = openmm.Context(system, integrator)
         context.setPositions(inpcrd.getPositions())
-        context.setPeriodicBoxVectors(inpcrd.getPeriodicBoxVectors())
+        if not implicit:
+            context.setPeriodicBoxVectors(inpcrd.getPeriodicBoxVectors())
+
         # Sample and store enthalpies
         enthalpies[phase] = np.zeros([niterations])
+
+        openmm.LocalEnergyMinimizer.minimize(context)
+
         for iteration in range(niterations):
-            integrator.step(nsteps)
+            integrator.step(nsteps_per_iteration)
             state = context.getState(getEnergy=True)
             potential_energy = state.getPotentialEnergy()
-            volume = state.getPeriodicBoxVolume()
-            enthalpies[phase][iteration] = (potential_energy + pressure*volume) / kT
+
+            if implicit:
+                enthalpies[phase][iteration] = potential_energy.value_in_unit(unit.kilojoules_per_mole)
+            else:
+                volume = state.getPeriodicBoxVolume()
+                enthalpy = potential_energy + (pressure * volume).in_unit_system(unit.si_unit_system) / unit.mole
+                enthalpies[phase][iteration] = enthalpy.value_in_unit(unit.kilojoules_per_mole)
+                #enthalpies[phase][iteration] = (potential_energy + pressure*volume) / kT
 
     # Use automatic equilibration detection and pymbar.timeseries to subsample
     from pymbar import timeseries
